14b872b9f5c766e61490d31e52b76f78c2593e51
yasirrhaq <yasir.haq98@gmail.com>
2024-10-29 19:28:01 +0700

    Add card for main screen & Create Navigator to move between page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/detail_screen.dart';
+import 'package:flutter_app/main_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wisata Bandung',
       theme: ThemeData(),
-      home: const DetailScreen(),
+      home: MainScreen(),
     );
   }
 }
